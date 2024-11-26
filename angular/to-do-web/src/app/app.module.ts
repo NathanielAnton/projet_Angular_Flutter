50f59abc0ad8f525cceab7e991c7f690637aa9b0
Nathaniel <antonhillarynathaniel@gmail.com>
2024-11-26 10:00:52 +0100

    add firestore + create/show

--- angular/to-do-web/src/app/app.module.ts
+++ angular/to-do-web/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './components/todo/todo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/to-do-web/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCN_yk8bYHpaWrzAkvyRScLP5ZVy9xdsPQ",
+    authDomain: "to-do-web-91a99.firebaseapp.com",
+    projectId: "to-do-web-91a99",
+    storageBucket: "to-do-web-91a99.firebasestorage.app",
+    messagingSenderId: "449787029937",
+    appId: "1:449787029937:web:a7e1377be9396fcee3f590",
+    measurementId: "G-1BLHQ5EYGX"
+  }
+};
