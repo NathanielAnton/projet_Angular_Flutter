1b0c3536e451bcda3a90fa32b832bf3cf297eae0
Nathaniel <antonhillarynathaniel@gmail.com>
2024-11-27 11:02:04 +0100

    affichage de la page d'accueil

--- flutter/to_do_flutter/lib/main.dart
+++ flutter/to_do_flutter/lib/main.dart
@@ -1,125 +1,257 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class Todo {
+  String title;
+  String description;
+  String status;
+  DateTime createdAt;
+
+  Todo({
+    required this.title,
+    required this.description,
+    required this.status,
+    required this.createdAt,
+  });
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: TodoListPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TodoListPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TodoListPageState createState() => _TodoListPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TodoListPageState extends State<TodoListPage> {
+  List<Todo> todosToDo = [];
+  List<Todo> todosInProgress = [];
+  List<Todo> todosDone = [];
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  String _selectedStatus = 'À faire';
 
-  void _incrementCounter() {
+  void openModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Ajouter une nouvelle tâche',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              TextField(
+                controller: _titleController,
+                decoration: InputDecoration(labelText: 'Titre'),
+              ),
+              TextField(
+                controller: _descriptionController,
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+              ),
+              DropdownButton<String>(
+                value: _selectedStatus,
+                onChanged: (value) {
+                  setState(() {
+                    _selectedStatus = value!;
+                  });
+                },
+                items: ['À faire', 'En cours', 'Terminé']
+                    .map((status) =>
+                        DropdownMenuItem(value: status, child: Text(status)))
+                    .toList(),
+                isExpanded: true,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    Todo newTodo = Todo(
+                      title: _titleController.text,
+                      description: _descriptionController.text,
+                      status: _selectedStatus,
+                      createdAt: DateTime.now(),
+                    );
+                    if (_selectedStatus == 'À faire') {
+                      todosToDo.add(newTodo);
+                    } else if (_selectedStatus == 'En cours') {
+                      todosInProgress.add(newTodo);
+                    } else {
+                      todosDone.add(newTodo);
+                    }
+                  });
+                  Navigator.pop(context);
+                },
+                child: Text('Créer'),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void deleteTodo(Todo todo) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (todo.status == 'À faire') {
+        todosToDo.remove(todo);
+      } else if (todo.status == 'En cours') {
+        todosInProgress.remove(todo);
+      } else {
+        todosDone.remove(todo);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Gestionnaire de Tâches'),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            ElevatedButton(
+              onPressed: () => openModal(context),
+              child: Text('+ Ajouter une tâche'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SizedBox(height: 20),
+            Text('Liste des Tâches',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+            Expanded(
+              child: Row(
+                children: [
+                  // Section "À faire"
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('À faire',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold)),
+                        ...todosToDo.isEmpty
+                            ? [
+                                Text('Aucune tâche pour le moment.',
+                                    style: TextStyle(color: Colors.grey))
+                              ]
+                            : todosToDo.map((todo) {
+                                return Card(
+                                  margin: EdgeInsets.only(bottom: 10),
+                                  elevation: 4,
+                                  child: ListTile(
+                                    title: Text(todo.title,
+                                        style: TextStyle(color: Colors.blue)),
+                                    subtitle: Text(todo.description),
+                                    trailing: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        IconButton(
+                                          icon: Icon(Icons.refresh),
+                                          onPressed: () {},
+                                        ),
+                                        IconButton(
+                                          icon: Icon(Icons.delete),
+                                          onPressed: () => deleteTodo(todo),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                      ],
+                    ),
+                  ),
+                  // Section "En cours"
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('En cours',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold)),
+                        ...todosInProgress.isEmpty
+                            ? [
+                                Text('Aucune tâche pour le moment.',
+                                    style: TextStyle(color: Colors.grey))
+                              ]
+                            : todosInProgress.map((todo) {
+                                return Card(
+                                  margin: EdgeInsets.only(bottom: 10),
+                                  elevation: 4,
+                                  child: ListTile(
+                                    title: Text(todo.title,
+                                        style: TextStyle(color: Colors.orange)),
+                                    subtitle: Text(todo.description),
+                                    trailing: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        IconButton(
+                                          icon: Icon(Icons.refresh),
+                                          onPressed: () {},
+                                        ),
+                                        IconButton(
+                                          icon: Icon(Icons.delete),
+                                          onPressed: () => deleteTodo(todo),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                      ],
+                    ),
+                  ),
+                  // Section "Terminé"
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Terminé',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold)),
+                        ...todosDone.isEmpty
+                            ? [
+                                Text('Aucune tâche pour le moment.',
+                                    style: TextStyle(color: Colors.grey))
+                              ]
+                            : todosDone.map((todo) {
+                                return Card(
+                                  margin: EdgeInsets.only(bottom: 10),
+                                  elevation: 4,
+                                  child: ListTile(
+                                    title: Text(todo.title,
+                                        style: TextStyle(color: Colors.green)),
+                                    subtitle: Text(todo.description),
+                                    trailing: IconButton(
+                                      icon: Icon(Icons.delete),
+                                      onPressed: () => deleteTodo(todo),
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
