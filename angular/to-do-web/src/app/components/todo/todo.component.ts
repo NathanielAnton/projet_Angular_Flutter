32973d599e50cc893b520fedd025372b52160847
Nathaniel <antonhillarynathaniel@gmail.com>
2024-11-29 10:38:14 +0100

    update for angular

--- angular/to-do-web/src/app/components/todo/todo.component.ts
+++ angular/to-do-web/src/app/components/todo/todo.component.ts
@@ -10,6 +10,15 @@ import * as bootstrap from 'bootstrap';
 })
 export class TodoComponent implements OnInit {
   @ViewChild('addTaskModal', { static: false }) addTaskModal!: ElementRef;
+  @ViewChild('editTaskModal', { static: false }) editTaskModal!: ElementRef;
+  selectedTodo: Todo = {
+    title: '',
+    description: '',
+    status: 0,
+    created_at: '',
+    updated_at: '',
+    id: ''
+  };
 
   openModal() {
     const modalElement = this.addTaskModal.nativeElement;
@@ -87,6 +96,32 @@ export class TodoComponent implements OnInit {
       });
     } else {
       console.error('L\'ID de la tâche est manquant');
+    }
+  }
+
+  editTodo(todo: Todo): void {
+    this.selectedTodo = { ...todo, status: Number(todo.status) };
+
+    const modalElement = this.editTaskModal.nativeElement;
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  }
+
+
+  updateTodo(): void {
+    this.selectedTodo.status = Number(this.selectedTodo.status);
+
+    this.selectedTodo.updated_at = new Date().toISOString();
+
+    if (this.selectedTodo.id) {
+      this.todoService.updateTodo(this.selectedTodo).then(() => {
+        console.log('Tâche mise à jour');
+        this.getTodos();
+      }).catch((error) => {
+        console.error('Erreur lors de la mise à jour de la tâche :', error);
+      });
+    } else {
+      console.error('ID de la tâche manquant');
     }
   }
 }
--- angular/to-do-web/src/app/services/todo.service.ts
+++ angular/to-do-web/src/app/services/todo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Todo } from '../models/todo.model';
 
 @Injectable({
@@ -12,7 +12,13 @@ export class TodoService {
   constructor(private firestore: AngularFirestore) { }
 
   getTodos(): Observable<Todo[]> {
-    return this.firestore.collection<Todo>(this.collectionName).valueChanges({ idField: 'id' });
+    return this.firestore.collection<Todo>(this.collectionName).snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Todo;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
   }
 
   createTodo(todo: Todo): Promise<void> {
@@ -30,5 +36,18 @@ export class TodoService {
     return this.firestore.collection(this.collectionName)
       .doc(todoId)
       .delete();
+  }
+
+  updateTodo(todo: Todo): Promise<void> {
+    if (!todo.id) {
+      return Promise.reject('ID du document manquant');
+    }
+
+    return this.firestore.collection(this.collectionName).doc(todo.id).update({
+      title: todo.title,
+      description: todo.description,
+      status: todo.status,
+      updated_at: todo.updated_at
+    });
   }
 }
